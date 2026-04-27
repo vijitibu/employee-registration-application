2c504fb5fbd9dcdaf1d4399016aedc9778e7a729
vijitibu <64551985+vijitibu@users.noreply.github.com>
2026-04-27 07:21:08 +0530

    Updated toaster responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Navbar from "./components/Employee/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Employees from "./Pages/Employees";
-import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+      <Toaster
+        position="top-right"
+        containerStyle={{
+          top: 20,
+          right: 20,
+          zIndex: 99999,
+        }}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: "12px",
+          },
+        }}
+      />
+
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-
         <Route path="/employees" element={<Employees />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
     </BrowserRouter>
   );
